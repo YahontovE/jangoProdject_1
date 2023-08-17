37ea90aa66801ddc6a1d94099d2af14bc8df8f8f
Evgen <yahontov-sport@inbox.ru>
2023-08-17 12:38:09 +0300

    add model in models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE={'blank':True,'null':True}
+
+class Category(models.Model):
+    name=models.CharField(max_length=100,verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    name=models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image=models.ImageField(upload_to='media/',**NULLABLE,verbose_name='Фото')
+    category=models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='Категория')
+    price=models.IntegerField(verbose_name='Цена')
+    date_of_creation=models.DateTimeField(**NULLABLE,verbose_name='Дата создания')
+    modified_date=models.DateTimeField(**NULLABLE,verbose_name='Дата правки')
+
+    def __str__(self):
+        return f'{self.name} {self.price} {self.description}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товавры'
